744fe59c10ea50c3c48cf3b32025751d49238c76
Mxster-dll <Mxster.dll@outlook.com>
2026-06-04 19:40:16 +0800

    1.5.3 修改了颜色插件显示的样式

--- src/java/com/mxster/everyphants/model/ColorPlugin.java
+++ src/java/com/mxster/everyphants/model/ColorPlugin.java
@@ -11,12 +11,26 @@ public class ColorPlugin extends Plugin<Color> {
     }
 
     public Result hexColorText(Color color) {
-        String colorText = color.toString().substring(2);
-        if (colorText.endsWith("ff")) {
-            colorText = colorText.substring(0, 6);
-        }
+        String hex = color.toString().substring(2);
 
-        return new Result("#" + colorText, null, 1, null);
+        String title = "#" +
+                (hex.endsWith("ff") ? hex.substring(0, 6) : hex);
+
+        double alpha = color.getOpacity() * 100;
+
+        int red = (int) Math.round(color.getRed() * 255);
+        int green = (int) Math.round(color.getGreen() * 255);
+        int blue = (int) Math.round(color.getBlue() * 255);
+
+        double hue = color.getHue();
+        double saturation = color.getSaturation() * 100;
+        double brightness = color.getBrightness() * 100;
+
+        String displayText = String.format(
+                "[%.0f%%]  #%s  ·  RGB %d %d %d  ·  HSB %.1f° %.1f%% %.1f%%",
+                alpha, hex, red, green, blue, hue, saturation, brightness);
+
+        return new Result(title, displayText, 1, null);
     }
 
     public Color parseWebColor(String s) {
