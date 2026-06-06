1556ced2259d6d9b970fed385e5f69522e7ec078
Mxster-dll <Mxster.dll@outlook.com>
2026-06-06 21:30:40 +0800

    3.2.12 恢复随机数插件的动态效果

--- src/java/com/mxster/everyphants/model/plugin/impl/RandomPlugin.java
+++ src/java/com/mxster/everyphants/model/plugin/impl/RandomPlugin.java
@@ -1,12 +1,18 @@
 package com.mxster.everyphants.model.plugin.impl;
 
 import java.math.BigInteger;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
+import com.mxster.everyphants.model.RefreshableResult;
 import com.mxster.everyphants.model.Result;
 import com.mxster.everyphants.model.plugin.core.ReactivePlugin;
 
 public class RandomPlugin extends ReactivePlugin<BigInteger> {
+
+    private final Map<BigInteger, RefreshableResult> cache = new ConcurrentHashMap<>();
+
     public RandomPlugin() {
         super("生成随机数");
 
@@ -17,11 +23,9 @@ public class RandomPlugin extends ReactivePlugin<BigInteger> {
     public BigInteger parseToUpperBound(String s) {
         try {
             var num = new BigInteger(s);
-
             if (num.compareTo(BigInteger.ZERO) <= 0) {
                 return null;
             }
-
             return num;
         } catch (Exception e) {
             return null;
@@ -29,14 +33,23 @@ public class RandomPlugin extends ReactivePlugin<BigInteger> {
     }
 
     public Result rand(BigInteger num) {
-        Random random = new Random();
+        return cache.computeIfAbsent(num, key -> {
+            String range = "随机数 [0, " + key.toString() + "]";
+            RefreshableResult result = new RefreshableResult(
+                    generate(key).toString(), range, 1, null);
+            result.withRefresh(0, () -> {
+                result.setTitle(generate(key).toString());
+            });
+            return result;
+        });
+    }
 
+    private static BigInteger generate(BigInteger bound) {
+        Random random = new Random();
         BigInteger result;
         do {
-            result = new BigInteger(num.bitLength(), random);
-        } while (result.compareTo(num) > 0);
-
-        String range = "随机数 [0, " + num.toString() + "]";
-        return new Result(result.toString(), range, 1, null);
+            result = new BigInteger(bound.bitLength(), random);
+        } while (result.compareTo(bound) > 0);
+        return result;
     }
 }
