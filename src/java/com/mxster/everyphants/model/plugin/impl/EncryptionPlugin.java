33877da65489166e68caacb30f69b3d5cd841ae9
Lumos <1454839682@qq.com>
2026-06-07 11:33:30 +0800

    3.6.2 添加了莫斯电码的加密功能

--- src/java/com/mxster/everyphants/model/plugin/impl/EncryptionPlugin.java
+++ src/java/com/mxster/everyphants/model/plugin/impl/EncryptionPlugin.java
@@ -23,6 +23,7 @@ public class EncryptionPlugin extends ReactivePlugin<String> {
 
         formatters.add(this::buildCaesar);
         formatters.add(this::buildFence);
+        formatters.add(this::buildMorseCode);
     }
 
     private static final int CAESAR_SHIFT = 3;
@@ -77,4 +78,75 @@ public class EncryptionPlugin extends ReactivePlugin<String> {
         }
     }
 
+    public static String morseCode(String s) {
+        String ans = "";
+        int l = s.length();
+        s = s.toUpperCase();
+
+        for (int i = 0; i < l; i++) {
+            ans += switch (s.charAt(i)) {
+                case 'A' -> ".-";
+                case 'B' -> "-...";
+                case 'C' -> "-.-.";
+                case 'D' -> "-..";
+                case 'E' -> ".";
+                case 'F' -> "..-.";
+                case 'G' -> "--.";
+                case 'H' -> "....";
+                case 'I' -> "..";
+                case 'J' -> ".---";
+                case 'K' -> "-.-";
+                case 'L' -> ".-..";
+                case 'M' -> "--";
+                case 'N' -> "-.";
+                case 'O' -> "--";
+                case 'P' -> ".--.";
+                case 'Q' -> "--.-";
+                case 'R' -> ".-.";
+                case 'S' -> "...";
+                case 'T' -> "-";
+                case 'U' -> "..-";
+                case 'V' -> "...-";
+                case 'W' -> ".--";
+                case 'X' -> "-..-";
+                case 'Y' -> "-.--";
+                case 'Z' -> "--..";
+                case '0' -> "-----";
+                case '1' -> ".----";
+                case '2' -> "...--";
+                case '3' -> "....-";
+                case '4' -> ".....";
+                case '5' -> "-....";
+                case '6' -> "--...";
+                case '7' -> "--...";
+                case '8' -> "---..";
+                case '9' -> "----.";
+                case '.' -> ".-.-.-";
+                case ':' -> "---...";
+                case ',' -> "--..--";
+                case ';' -> "-.-.-.";
+                case '?' -> "..--..";
+                case '=' -> "-...-";
+                case '\'' -> "-..-.";
+                case '/' -> "-..-.";
+                case '!' -> "-.-.--";
+                case '-' -> "-....-";
+                case '_' -> "..--.-";
+                case '"' -> ".-..-.";
+                case '(' -> "-.--.";
+                case ')' -> "-.--.-";
+                case '$' -> "...-..-";
+                case '&' -> "....";
+                case '@' -> ".--.-.";
+                default -> null;
+            };
+        }
+
+        return ans;
+    }
+
+    public Result buildMorseCode(String s) {
+        return new Result(fence(s), "摩斯电码", 0.5, null);
+
+    }
 }
